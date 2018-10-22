0038e4bab304df622dc615a58d341831f2dc5f52
qtrinh128 <qtrinh128@gmail.com>
2018-10-22 09:51:59 +0700

    them file cau hinh db

---
+++ config/env/dbconfig.js
@@ -0,0 +1,9 @@
+module.exports = dbConfig = {
+  user: "pgnotify",
+  password: "pgnotify@123",
+  server: "210.211.124.16",
+  database: "PasGo-Notify-Setting",
+  options: {
+    encrypt: false
+  }
+}
